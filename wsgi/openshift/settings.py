1ffdb7ff226927427893fc203af2bbb4adf35cf2
Diego M. Pérez <dmperez@utn.edu.ec>
2014-01-29 01:29:07 -0500

    Update settings.py

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -82,7 +82,8 @@ MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.environ.get('STATIC_ROOT',os.path.join(os.path.dirname(__file__), 'static'))
+#STATIC_ROOT = os.environ.get('STATIC_ROOT',os.path.join(os.path.dirname(__file__), 'static'))
+STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
