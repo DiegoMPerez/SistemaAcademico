f475aa11bba8270bbb87f8f9127ef1af6ba829b7
DiegoMPerez <dmperez@utn.edu.ec>
2014-01-29 21:17:21 -0500

    plantillas

--- wsgi/openshift/settings.py
+++ wsgi/openshift/settings.py
@@ -71,22 +71,29 @@ USE_L10N = True
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
 
-MEDIA_ROOT = os.environ.get('MEDIA_ROOT',os.path.join(os.path.dirname(__file__), 'media'))
+#MEDIA_ROOT = os.environ.get('MEDIA_ROOT',os.path.join(os.path.dirname(__file__), 'media'))
+MEDIA_ROOT = os.environ.get('OPENSHIFT_DATA_DIR', '')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
+
+#MEDIA_URL = os.environ.get('MEDIA_URL', '/media/')
+MEDIA_URL = ''
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.environ.get('STATIC_ROOT',os.path.join(os.path.dirname(__file__), 'static'))
 
+#STATIC_ROOT = os.environ.get('STATIC_ROOT',os.path.join(os.path.dirname(__file__), 'static'))
+STATIC_ROOT = os.path.join(PROJECT_DIR, '..', 'static')
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = os.environ.get('STATIC_URL', '/static/')
+
+#STATIC_URL = os.environ.get('STATIC_URL', '/static/')
+STATIC_URL = '/static/'
+
 
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
@@ -158,6 +165,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
     'demo',
     'modulo12',
+    'bootstrap3',
 )
 
 # A sample logging configuration. The only tangible logging
